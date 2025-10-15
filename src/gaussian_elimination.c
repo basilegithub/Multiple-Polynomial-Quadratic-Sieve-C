604650383a6b63289d00c9f35b95e1c0b19bf8ba
basilegithub <basile.jezequel@gmail.com>
2025-10-15 11:21:47 +0200

    Finalized gaussian_elimination.c cleaning. No source of the bug identified yet

--- src/gaussian_elimination.c
+++ src/gaussian_elimination.c
@@ -4,11 +4,10 @@
 
 void gaussian_elimination(unsigned long relations_len, unsigned long base_size, mpz_t *dense_matrix, mpz_t *res)
 {
-    mpz_t tmp, tmp2;
-    mpz_inits(tmp, tmp2, NULL);
+    mpz_t tmp;
+    mpz_init(tmp);
 
-    unsigned long tmp_long;
-    unsigned long i;
+    unsigned long row_index;
     int is_zero_bit;
 
     mpz_t * restrict matrix = dense_matrix;
@@ -18,53 +17,35 @@ void gaussian_elimination(unsigned long relations_len, unsigned long base_size, 
 
     for (size_t i = 0 ; i < relations_len ; i++)
     {
-        mpz_mul_2exp(tmp2, tmp, relations_len-i-1);
-        mpz_set(result[i], tmp2);
+        mpz_mul_2exp(result[i], tmp, relations_len-i-1);
     }
 
     unsigned long pivot_row = 0;
 
     for (size_t j = 0 ; j < base_size ; j++)
     {
-        i = pivot_row;
-        
-        mpz_div_2exp(tmp, matrix[i], j);
-        mpz_set_ui(tmp2, 1);
-        mpz_and(tmp, tmp, tmp2);
-        is_zero_bit = (!mpz_cmp_ui(tmp, 0));
+        row_index = pivot_row;
+        is_zero_bit = (!mpz_tstbit(matrix[row_index], j));
 
-        mpz_set_ui(tmp2, 1);
-
-        while (i < relations_len && is_zero_bit)
+        while (row_index < relations_len && is_zero_bit)
         {
-            i++;
-
-            mpz_div_2exp(tmp, matrix[i], j);
-            mpz_and(tmp, tmp, tmp2);
-            is_zero_bit = (!mpz_cmp_ui(tmp, 0));
+            row_index++;
+            is_zero_bit = (!mpz_tstbit(matrix[row_index], j));
         }
 
-        if (i < relations_len && !is_zero_bit) // We have found the pivot row
+        if (row_index < relations_len && !is_zero_bit) // We have found the pivot row
         {
-            if (i != pivot_row) // If needed, permute the rows so that pivot is at the top
+            if (row_index != pivot_row) // If needed, permute the rows so that pivot is at the top
             {
-                mpz_set(tmp, matrix[i]);
-                mpz_set(matrix[i], matrix[pivot_row]);
-                mpz_set(matrix[pivot_row], tmp);
-
-                mpz_set(tmp, result[i]);
-                mpz_set(result[i], result[pivot_row]);
-                mpz_set(result[pivot_row], tmp);
+                mpz_swap(matrix[row_index], matrix[pivot_row]);
+                mpz_swap(result[row_index], result[pivot_row]);
             }
 
-            for (size_t k = i+1 ; k < relations_len ; k++) // reduce all the lower rows so that coefficient dense_matrix[k][j] = 0
+            for (size_t k = row_index+1 ; k < relations_len ; k++) // reduce all the lower rows so that coefficient dense_matrix[k][j] = 0
             {
-                mpz_div_2exp(tmp, matrix[k], j);
-                mpz_set_ui(tmp2, 1);
-                mpz_and(tmp, tmp, tmp2);
-                is_zero_bit = (!mpz_cmp_ui(tmp, 1));
+                is_zero_bit = (!mpz_tstbit(matrix[k], j));
 
-                if (is_zero_bit)
+                if (!is_zero_bit)
                 {
                     mpz_xor(matrix[k], matrix[k], matrix[pivot_row]);
                     mpz_xor(result[k], result[k], result[pivot_row]);
@@ -73,7 +54,7 @@ void gaussian_elimination(unsigned long relations_len, unsigned long base_size, 
             pivot_row++;
         }
     }
-    mpz_clears(tmp, tmp2, NULL);
+    mpz_clear(tmp);
 }
 
 bool row_is_zero(unsigned long relations_len, unsigned long base_size, mpz_t *dense_matrix, size_t row_index)
